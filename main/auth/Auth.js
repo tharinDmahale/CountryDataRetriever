85f72e154d0bd6b56960e499caf5413d2756fdf3
tharinDmahale <deelaka.mahale@gmail.com>
2025-04-10 20:51:02 +0530

    upd auth response

--- main/auth/Auth.js
+++ main/auth/Auth.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const AuthUtil = require("./AuthUtil");
+const Response = require("../common/Response.json");
 
 class Auth {
     static async authenticate(req, res, next) {
@@ -8,14 +9,22 @@ class Auth {
             const auth = req.header("authorization");
 
             if (!auth) {
-                res.status(401).send("Access token is missing!");
+                Response.data = {
+                    error: "Access token is missing!"
+                }
+
+                res.status(401).send(Response);
 
             } else {
                 const tokencontent = auth.split(" ")[1];
                 const verified = await AuthUtil.verifyAccessToken(tokencontent);
 
                 if (!verified) {
-                    res.status(401).send("Invalid access token!");
+                    Response.data = {
+                        error: "Invalid access token!"
+                    }
+                    
+                    res.status(401).send(Response);
                     
                 } else {
                     next();
